2a6c56eeb464c7f4a9fb9c70cc2ea1fe0f19f92f
Peter Witwer <pwitwer@icloud.com>
2020-10-04 19:01:32 -0600

    added .txt output

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -1,8 +1,9 @@
 #This will be the main file for the pybank challenge
 #Imports
-
 import os
 import csv
+import sys
+import subprocess
 
 #define variables to store values in
 net_profit = 0.0
@@ -15,6 +16,8 @@ lastmonth = 0
 current_change = 0
 date1 = ""
 date2 = ""
+
+original_stdout = sys.stdout #saving a reference to original standard output
 
 
 bdata_csv = os.path.join('Resources', 'budget_data.csv')
@@ -48,8 +51,25 @@ with open(bdata_csv, newline='') as csvfile:
         #update lastmonth
         lastmonth = int(row[1])
         
-
+    #calculate average change, confused where the example on gitlab got the average: 2315.12
+    #would like to know what I did wrong if this isn't correct
     average_change = sum(average_change_list)/len(average_change_list)
+
+    #changed stdout to create a txt file with the output I want
+    with open('Financial_Analysis.txt', 'w') as o:
+        sys.stdout = o
+        print(' ')
+        print('Financial Analysis')
+        print('--------------------------------------------')
+        print('Total Months: ' + str(len(average_change_list)+1))
+        print('Total: ' + str(net_profit))
+        print('Average change: ' + str(round(average_change, 2)))
+        print('Greatest increase in profits: ' + date1 + ' ' + str(greatest_increase))
+        print('Greatest decrease in profits: ' + date2 + ' ' + str(greatest_decrease))
+        print(' ')
+        sys.stdout = original_stdout
+
+    #print normal output
     print(' ')
     print('Financial Analysis')
     print('--------------------------------------------')
@@ -59,8 +79,6 @@ with open(bdata_csv, newline='') as csvfile:
     print('Greatest increase in profits: ' + date1 + ' ' + str(greatest_increase))
     print('Greatest decrease in profits: ' + date2 + ' ' + str(greatest_decrease))
     print(' ')
-    #print(sum(average_change_list))
-    #print(average_change_list)
 
 
    
